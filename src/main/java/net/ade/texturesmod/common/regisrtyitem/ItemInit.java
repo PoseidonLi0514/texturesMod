0cd253bb98bb8a7fcb333e1df8a061ef1215f5ab
adehcby <3533258769@qq.com>
2021-08-29 04:40:58 +0800

    update itemregistry

--- src/main/java/net/ade/texturesmod/common/regisrtyitem/ItemInit.java
+++ src/main/java/net/ade/texturesmod/common/regisrtyitem/ItemInit.java
@@ -7,6 +7,9 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+/**
+ * @author lhy
+ */
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
 
